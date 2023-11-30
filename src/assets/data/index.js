c6f3ce86af49b9d6fac8a6d18c616a3ff582deb0
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2023-11-30 10:16:15 -0800

    LNwav 2

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,11 +1,11 @@
 const Data = [
     {
         id: 1,
-        title: '1000 Miles Away - Z3',
-        artist: 'Z3',
-        thumbnail: require('../images/img01.jpg'),
-        qr: require('../qrCodes/qr01.png'),
-        src: require('../music/1000milesAway.mp3')
+        title: 'Toxic Maximalist - The Orange Pill Project',
+        artist: 'The Orange Pill Project',
+        thumbnail: require('../images/img03.jpg'),
+        qr: require('../qrCodes/qr03.png'),
+        src: require('../music/ToxicMaximalist.mp3')
     },
     {
         id: 2,
@@ -14,14 +14,6 @@ const Data = [
         thumbnail: require('../images/img02.jpg'),
         qr: require('../qrCodes/qr02.png'),
         src: require('../music/REALITY.mp3')
-    },
-    {
-        id: 3,
-        title: 'Toxic Maximalist - The Orange Pill Project',
-        artist: 'The Orange Pill Project',
-        thumbnail: require('../images/img03.jpg'),
-        qr: require('../qrCodes/qr03.png'),
-        src: require('../music/ToxicMaximalist.mp3')
     },
 ]
 
